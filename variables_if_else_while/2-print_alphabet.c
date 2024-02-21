e3a99d476cd280e457a621f24b756d09f2a651ea
Guillom8769 <8769@holbertonstudents.com>
2024-02-21 10:57:11 +0100

    prints the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main prints the alphabet in lowercase
+ * Return: always 0
+ */
+int main(void)
+{
+
+	char c = 'a';
+
+	while (c <= 'z')
+
+	{
+		putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return (0);
+}

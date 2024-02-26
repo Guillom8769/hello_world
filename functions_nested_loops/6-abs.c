0417db64f86bb8d606791b0a7f2e8a9acd9b80ff
Guillom8769 <8769@holbertonstudents.com>
2024-02-26 19:37:57 +0100

    unction that computes the absolute value of an integer

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -3,7 +3,7 @@
 /**
  * print_sign - function that checks for number
  * @n : entier
- * Return: 1 (>0) -1 (<0) or 0 (==0)
+ * Return: absolute value 
  */
 
 int print_sign(int n)
@@ -18,6 +18,7 @@ int print_sign(int n)
 	}
 	else
 	{
-		return (0);
+		return (n);
 	}
+	return (0)
 }

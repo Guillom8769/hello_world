0062746f9138a567a864c34f9139e6d23ea1bc3d
Guillom8769 <8769@holbertonstudents.com>
2024-02-27 18:00:49 +0100

    function that prints 10 times the numbers, from 0 to 14, followed by a new line.

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,8 +3,6 @@
 /**
  * more_numbers: function that prints 10 times the numbers,
  *               from 0 to 14, followed by a new line.
- *@i: integer
- *@j: integer
  */
 
 void more_numbers(void)

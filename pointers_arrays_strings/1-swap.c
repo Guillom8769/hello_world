90d829b811e8c070726c4f840ec7d6eb2959631b
Guillom8769 <8769@holbertonstudents.com>
2024-02-28 21:04:38 +0100

    function that swaps the values of two integers

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * reset_to_98 - pointer to an int as parameter and updates the value to 98.
+  * @n : pointer n
+  */
+
+void swap_int(int *a, int *b)
+{
+	*a = &nombreA;
+	*b = &nombreB;
+
+	swap_int(a, b);
+}

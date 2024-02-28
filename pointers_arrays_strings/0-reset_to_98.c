278966e4cc85bbed3365c8984b57a7e2e53a405e
Guillom8769 <8769@holbertonstudents.com>
2024-02-28 19:46:10 +0100

    unction that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_square:function prints a square
+ *@size: nb de caractère a imprimer
+ *@i: integer
+ */
+
+void reset_to_98(int *n)
+{
+	int * ad ;
+	int n ;
+	n = 0 ;
+	ad = &n ;
+	*ad = 98 ;
+}

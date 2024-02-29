3fcebba206bab51ca2575767a92f74f87a6ae2f6
Guillom8769 <8769@holbertonstudents.com>
2024-02-29 17:43:29 +0100

    unction that reverses a string.

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_rev - prints a string in reverse
+ * @s: string to print
+ * Description: prints a string in reverse
+ */
+
+ void rev_string(char *s);
+{
+	int l = 0;
+	int i = 0;
+
+	while (s[i++])
+		l++;
+	for (i = l - 1; i >= 0; i--)
+		_putchar(s[]);
+		_putchar(s[i]);
+
+	_putchar('\n');
+}
+
+

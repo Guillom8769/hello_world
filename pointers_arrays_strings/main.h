1fefa70438905be0108f88d9698eb4eeb107d385
Guillom8769 <8769@holbertonstudents.com>
2024-03-05 13:44:18 +0100

    Write a function that prints the chessboard.

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -87,3 +87,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 
 char *_strchr(char *s, char c);
+
+char *_strstr(char *haystack, char *needle);
+
+    void print_chessboard(char (*a)[8]);
+
+

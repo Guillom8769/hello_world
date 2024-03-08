f0fa6249084a722c94b35f27ea036679d1860baa
Guillom8769 <8769@holbertonstudents.com>
2024-03-08 17:57:35 +0100

    C - argc, argv

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+*main - print prod of number of arg
+*@argc: size of argv
+*@argv: array of program
+*Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	int num1, num2;
+
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	printf("%d\n", num1 * num2);
+
+	return (0);
+}

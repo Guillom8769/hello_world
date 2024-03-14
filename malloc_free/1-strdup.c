9573640f40ee3bc010a1a3105b56e059f7938c00
Guillom8769 <8769@holbertonstudents.com>
2024-03-14 16:21:26 +0100

    C - malloc, free

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicate the string
+ * @str : string
+ *
+ * Return: /
+ */
+
+char *_strdup(char *str)
+{
+	char *sortie;
+	unsigned int index1;
+	unsigned int index2;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (index1 = 0; str[index1] != '\0'; index1++);
+
+	sortie = (char *)malloc(sizeof(char) * (index1 + 1));
+
+	if (sortie == NULL)
+		return (NULL);
+
+	for (index2 = 0; index2 <= index1; index2++)
+		sortie[index2] = str[index2];
+
+	return (sortie);
+}
